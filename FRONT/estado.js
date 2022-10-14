98bd478cc0d4f94c4404e8ab8e3f6e80d0d5c8d7
p3rsidh <persidh.pimenta@gmail.com>
2022-10-14 15:41:55 -0300

    atualizações

--- FRONT/estado.js
+++ FRONT/estado.js
@@ -3,8 +3,8 @@ const table = document.getElementById("estados")
 const botaoMostrar = document.getElementById("mostrar")
 
 
-function mostrar(){
-fetch("http://localhost:8080/estados").then((data)=>{
+table.addEventListener('click', event => {event.preventDefault();
+    fetch('http://localhost:8080/estados').then((data)=>{
     return data.json();
   }).then((todosUsuarios) =>{
       let data1 = "";
@@ -13,17 +13,17 @@ fetch("http://localhost:8080/estados").then((data)=>{
           <thead>
               <tr>
                   <td scope="col">${values.codigo} </td>
-                  <td scope="col">${values.nomeEstado}</td> 
+                  <td scope="col">${values.nome_estado}</td> 
                   
               </tr>
           </thead>
           `
       })
-     
-      console.log( document.getElementById("estados").replaceWith(data1));
+      document.getElementById("estados").replaceWith(data1)
+      
     }).catch(e => console.log("Deu erro: " + e))
 
-}
+})
 
-botaoMostrar.onclick = mostrar();
+
 
